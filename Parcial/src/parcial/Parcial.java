919fcb45c60763c073af25062daa9fe769d03610
luisa fer <luisa fer@DESKTOP-RDQTS6F>
2019-10-10 09:36:25 -0500

    Adicción del proyecto Arquitectura de datos

---
+++ Parcial/src/parcial/Parcial.java
@@ -0,0 +1,14 @@
+
+package parcial;
+
+/**
+ *
+ * @author luisa fer
+ */
+public class Parcial {
+
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
